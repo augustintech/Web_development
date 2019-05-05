9ceab3a565481f086ede4b72b34ac1732cc89aa2
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-05-05 15:37:01 +0100

    Maths/operators

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,7 @@ console.log(12 % 5);
 console.log('Andrew');
 console.log("Great " + "thing");
 console.log('You can\'t believe me');
+console.log(5 > 8);
+console.log(9 === 9);
+console.log(4!== 4);
+// NanN: Not a Number
