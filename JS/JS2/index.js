e79925ce4c41ffe6e61c6a4f6aed21a19ceb3202
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-06-14 21:55:09 +0100

    Variables continued. With some user interaction

--- JS/JS2/index.js
+++ JS/JS2/index.js
@@ -1,2 +1,10 @@
 var blue = "This looks good" + "!";
 console.log(blue);
+
+
+var first = prompt("enter first number");
+var second = prompt("enter second number");
+
+sum = Number(first) + Number(second);
+console.log(sum);
+alert("The sum is: " + sum);
