f7617274090a402d32204c0d6293a5c7a90affa5
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-06-14 22:19:39 +0100

    Making a calculator that responds to user input

--- JS/JS2/index.js
+++ JS/JS2/index.js
@@ -8,3 +8,17 @@ var second = prompt("enter second number");
 sum = Number(first) + Number(second);
 console.log(sum);
 alert("The sum is: " + sum);
+
+var a = true;
+console.log(a)
+a = "Hello"
+console.log(a)
+
+var b;
+console.log(b)
+
+FirstNumber = prompt()
+console.log(FirstNumber);
+SecondNumber = prompt();
+console.log(SecondNumber);
+alert(Number(FirstNumber) + Number(SecondNumber));
