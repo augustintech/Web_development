b11462ab53f066eeeae052a1d3d5bf99eb1640c6
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-06-15 23:08:37 +0100

    Exercise

--- JS/JS3/index.js
+++ JS/JS3/index.js
@@ -42,3 +42,13 @@ var name = "Bob";
 if (!(name === "Bob")) {
     alert("Hi Bob");
 }
+
+
+age = prompt("How old are you?"); 
+if (Number(age) < 18 ) {
+    alert("Sorry, you are too young to drive this car. Powering off")
+} else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!")
+} else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
