218087e2b65d56c8c1e1aca490742b9a9ed64e64
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-06-15 22:44:27 +0100

    else if statements and && statement

--- JS/JS3/index.js
+++ JS/JS3/index.js
@@ -2,9 +2,37 @@ var name = "Billy";
 if (name === "Billy") {
     alert("hi Billy!");
 }
-var name = "Susan"
+var name = "Susan";
 if (name === "Billy") {
     alert("hi Billy!");
 } else {
     alert("hmmm I don't know you")
+}
+
+var name = "Susy";
+
+if (name === "Billy") {
+    alert("hi Billy");
+} else if (name === "Susy") {
+    alert("hi Susy!");
+} else {
+    alert("I don't know you");
+}
+
+var name = "Ann";
+
+if (name === "Billy" || name === "Ann") {
+    alert("Hi Billy or Ann!");
+}
+
+if (name === "Billy" && name === "Ann") {
+    alert("Hi Billy or Ann!");
+}
+
+
+var firstName = "Bob";
+var lastName = "Smith";
+
+if (firstName === "Bob" && lastName === "Smith") {
+    alert("Hi Bob Smith!");
 }
