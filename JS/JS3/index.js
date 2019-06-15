86329d0022c77a6c559b9ce3bbc1ee8b58d3fec0
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-06-15 22:31:49 +0100

    If/else statements

---
+++ JS/JS3/index.js
@@ -0,0 +1,10 @@
+var name = "Billy";
+if (name === "Billy") {
+    alert("hi Billy!");
+}
+var name = "Susan"
+if (name === "Billy") {
+    alert("hi Billy!");
+} else {
+    alert("hmmm I don't know you")
+}
