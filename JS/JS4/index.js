6034f5313999b97eceae56a17adf45b8152d95c4
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-06-16 22:27:50 +0100

    Testing out functions

--- JS/JS4/index.js
+++ JS/JS4/index.js
@@ -4,3 +4,20 @@ function test() {
 
 test();
 
+var sayHello = function() {
+    console.log("Test");
+}
+sayHello()
+
+function yellow() {
+    console.log("first try");
+    console.log("violet");
+}
+yellow();
+
+function yellow2() {
+    console.log("orange");
+    console.log("blue");
+}
+
+yellow2();
