ed128e8acaa1b6c68bad2de2c4b537a7a081ce72
greenLight8 <44822931+greenLight8@users.noreply.github.com>
2019-07-01 23:27:08 +0100

    Update exercise.js

--- JS/JS7/exercise.js
+++ JS/JS7/exercise.js
@@ -1,1 +1,13 @@
+var goals = [ 
+    “Finish lesson X”
+    “Get married”
+    “Run a marathon”
+    “Grow lettuce”
+    “Implement changes”
+];
+
+for (var i=0; i < goals.length; i++) {
+    goals[i] = goals[i] + “!”
+}
+
 
