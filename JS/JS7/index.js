7d686e87a80fa917002bbae9a2ce077d503e21b3
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-07-02 17:12:21 +0100

    forEach method - extended

--- JS/JS7/index.js
+++ JS/JS7/index.js
@@ -6,15 +6,33 @@ var goals = [
     "Grow lettuce",
     "Implement changes"
 ];
+
+
+var goalsImportant = [ 
+    "Finish lesson X!",
+    "Get married!",
+    "Run a marathon!",
+    "Grow lettuce!",
+    "Implement changes!"
+];
+
+
 var goalsLength = goals.length;
-for (var i=0; i < goalsLength; i++) {
-   console.log(goals[i], i);
+
+//for (var i=0; i < goalsLength; i++) {
+//   console.log(goals[i], i);
+//}
+
+//goals.forEach(function(goals, i) {
+//    console.log(goals, i);
+//}) 
+
+function logGoals(goals, i) {
+    console.log(goals, i);
 }
 
-goals.forEach(function(goals, i) {
-    console.log(goals, i);
-}) 
-
+goals.forEach(logGoals);
+goalsImportant.forEach(logGoals);
 
 /* var counterOne = 10;
 while (counterOne > 0) {
