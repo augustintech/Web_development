3c373fb5da754e2b7fd83a56f2c8e9043825ac66
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-07-02 16:59:24 +0100

    do while loop

--- JS/JS7/index.js
+++ JS/JS7/index.js
@@ -12,8 +12,14 @@ var goalsLength = goals.length;
 //}
 
 
-var counterOne = 10;
-while (counterOne > 0) {
-    console.log(counterOne);
-    counterOne--;
-}
+//var counterOne = 10;
+//while (counterOne > 0) {
+//    console.log(counterOne);
+//   counterOne--;
+//}
+
+var counterTwo = 10
+do {
+    console.log(counterTwo)
+    counterTwo--;
+} while (counterTwo > 0);
