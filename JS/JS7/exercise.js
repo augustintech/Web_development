25b829d28204df784b790394fd00be1c0c8b34e7
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-07-02 16:56:48 +0100

    Loops

--- JS/JS7/exercise.js
+++ JS/JS7/exercise.js
@@ -1,13 +1,1 @@
-var goals = [ 
-    “Finish lesson X”
-    “Get married”
-    “Run a marathon”
-    “Grow lettuce”
-    “Implement changes”
-];
-
-for (var i=0; i < goals.length; i++) {
-    goals[i] = goals[i] + “!”
-}
-
 
--- JS/JS7/index.js
+++ JS/JS7/index.js
@@ -1,1 +1,19 @@
 
+var goals = [ 
+    "Finish lesson X",
+    "Get married",
+    "Run a marathon",
+    "Grow lettuce",
+    "Implement changes"
+];
+var goalsLength = goals.length;
+//for (var i=0; i < goalsLength; i++) {
+//    goals.pop();
+//}
+
+
+var counterOne = 10;
+while (counterOne > 0) {
+    console.log(counterOne);
+    counterOne--;
+}
